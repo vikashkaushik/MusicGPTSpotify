c3204d9317285ede8d86c5270a446bb300ed783e
vikashkaushik <86675397+vikashkaushik@users.noreply.github.com>
2023-06-20 20:33:55 -0500

    spotify api

--- songData.py
+++ songData.py
@@ -1,14 +1,26 @@
+import spotipy  
+from spotipy.oauth2 import SpotifyOAuth  
+from spotipy import Spotify  
+  
+sp_oauth = SpotifyOAuth(client_id="6ccc82325a60410c9d0bbc5a7014537e", client_secret="5cac035e1acb4ba4acc94f014ee768ad", redirect_uri="http://localhost:8000", scope="user-soa-link")  
+  
+access_token = sp_oauth.get_access_token()  
+refresh_token = sp_oauth.get_refresh_token()
 
-import pandas as pd
-import openai
+sp = Spotify(auth_manager=sp_oauth)  
+  
+results = sp.search(q='track:dancing', type='track')   
 
+track_id = '4uLU6hMCjMI75M1A2tKUQC'  
+audio_features = sp.audio_features(track_id)
 
-categories = ['song_name', 'genre', 'danceability', 'energy', 'key', 'loudness', 'mode', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo']
-
-songData = pd.read_json('genres.json')
-print(songData['danceability'][1])
-
-labels = [songData[x].split('.')[-1] for x in songData['danceability']]
-texts = [text.strip() for text in songData['energy']]
-df = pd.DataFrame(zip(texts, labels), columns = ['prompt','completion']) #[:300]
-df.head()
+# # Create a playlist  
+# playlist_name = "My new playlist"  
+# sp.user_playlist_create("USERNAME", playlist_name)  
+  
+# #Add tracks to the playlist  
+# track_ids = ['4uLU6hMCjMI75M1A2tKUQC', '1301WleyT98MSxVHPZCA6M']  
+# sp.user_playlist_add_tracks("USERNAME", playlist_id, track_ids)  
+  
+# # Retrieve all the playlists of a user  
+# playlists = sp.user_playlists("USERNAME")  
