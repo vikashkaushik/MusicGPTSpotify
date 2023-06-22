f408df33916e64564e51a1a52bc364e9a62bcedf
vikashkaushik <86675397+vikashkaushik@users.noreply.github.com>
2023-06-22 18:12:11 -0500

    bruh

--- songData.py
+++ songData.py
@@ -1,19 +1,63 @@
+from flask import Flask, request, url_for, session, redirect
 import spotipy  
 from spotipy.oauth2 import SpotifyOAuth  
 from spotipy import Spotify  
-  
-sp_oauth = SpotifyOAuth(client_id="6ccc82325a60410c9d0bbc5a7014537e", client_secret="5cac035e1acb4ba4acc94f014ee768ad", redirect_uri="http://localhost:8000", scope="user-soa-link")  
-  
-access_token = sp_oauth.get_access_token()  
-refresh_token = sp_oauth.get_refresh_token()
+import time
 
-sp = Spotify(auth_manager=sp_oauth)  
-  
-results = sp.search(q='track:dancing', type='track')   
+app = Flask(__name__)
 
-track_id = '4uLU6hMCjMI75M1A2tKUQC'  
-audio_features = sp.audio_features(track_id)
+app.secret_key = "fdskjfdsnsdk"
+app.config['SESSION_COOKIE_NAME'] = 'VK Cookie'
+TOKEN_INFO = "token_info"
 
+@app.route('/')
+def login():
+    sp_oauth = create_spotify_oauth()
+    auth_url = sp_oauth.get_authorize_url()
+    return redirect(auth_url)
+
+@app.route('/redirect')
+def redirectPage():
+    sp_oauth = create_spotify_oauth()
+    session.clear()
+    code = request.args.get('code')
+    token_info = sp_oauth.get_access_token(code)
+
+    session[TOKEN_INFO] = token_info
+    return redirect(url_for('getTracks', _external=True))
+
+@app.route('/getTracks')
+def getTracks():
+    try:
+        token_info = get_token()
+    except:
+        print("user not logged in")
+        redirect(url_for('login', _external=False))
+        
+    sp = spotipy.Spotify(auth=token_info['access_token'])
+    return sp.current_user_saved_tracks(limit=50, offset=0)
+    
+
+
+def get_token():
+    token_info = session.get(TOKEN_INFO, None)
+    if not token_info:
+        raise "exception"
+    now = int(time.time())
+
+    is_expired = token_info['expires_at'] - now < 60
+    if (is_expired):
+        sp_oauth = create_spotify_oauth()
+        token_info = sp_oauth.refresh_access_token(token_info['refresh_token'])
+        return token_info
+
+def create_spotify_oauth():
+    return SpotifyOAuth(
+        client_id="6ccc82325a60410c9d0bbc5a7014537e",
+          client_secret="5cac035e1acb4ba4acc94f014ee768ad",
+            redirect_uri=url_for('redirectPage', _external=True),
+              scope="user-library-read"
+    )
 # # Create a playlist  
 # playlist_name = "My new playlist"  
 # sp.user_playlist_create("USERNAME", playlist_name)  
